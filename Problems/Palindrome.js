e58b8bdf37fa066a056f9b385de88ec9a6a5c313
Abhishek <eduandabhi@gmail.com>
2022-01-08 16:09:35 +0530

    Palindrome Check

---
+++ Problems/Palindrome.js
@@ -0,0 +1,16 @@
+let number= prompt("Enter a number");
+
+let sum=0;
+let temp=number;
+let r;
+
+while(number>0){
+    r=number%10;
+    sum=sum*10+r;
+    number=parseInt(number/10);
+}
+if(temp==sum){
+    console.log(temp +' is Palindrom Number')
+}else{
+    console.log(temp+ ' is not a Palindrom number')
+}
