364a82a7280779bf0d9f90c638ad89bd84ddbe95
Abhishek <eduandabhi@gmail.com>
2022-03-27 17:50:56 +0530

    Saving game state

--- Problems/practice.js
+++ Problems/practice.js
@@ -1,20 +1,11 @@
-//ArmStrong check
-//153=1*1*1+5*5*5+3*3*3=153
+let name = "Abhi";
 
-let n = 151;
-let numberOfDigits = n.toString().length;
-let sum = 0;
+let reversed = "";
 
-let tem = n;
-
-while (tem > 0) {
-  let r = tem % 10;
-  sum = sum + r ** numberOfDigits;
-  tem = parseInt(tem / 10);
-}
-
-if (n === sum) {
-  console.log(`${n} is Armstrong Number`);
-} else {
-  console.log(`${n} is not Armstrong Number`);
-}
+const rev = (name) => {
+  for (let character of name) {
+    reversed = character + reversed;
+  }
+  return reversed;
+};
+console.log(rev(name));
