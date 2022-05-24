c696bb4f1e6965da5f437e1e483bccd825800ff9
Abhishek <eduandabhi@gmail.com>
2022-05-24 14:16:05 +0530

    Longest Common Prefix

---
+++ leetcodes/Arrays/PlusOne.js
@@ -0,0 +1,15 @@
+let digits = [9];
+
+var plusOne = () => {
+  for (let i = digits.length - 1; i >= 0; i--) {
+    digits[i]++;
+    if (digits[i] > 9) {
+      digits[i] = 0;
+    } else {
+      return digits;
+    }
+  }
+  digits.unshift(1);
+  return digits;
+};
+console.log(plusOne());
---
+++ leetcodes/LongestCommonPrefix.js
@@ -0,0 +1,32 @@
+let input = ["flower", "flow", "floight"];
+// # First solution using Brute Fource
+// var longestCommonPrefix = () => {
+//   let firstWord = input[0];
+//   let result = "";
+//   for (let i = 0; i < firstWord.length; i++) {
+//     let char = firstWord[i];
+//     let counter = 0;
+//     for (let j = 1; j < input.length; j++) {
+//       let word = input[j];
+//       if (char === word[i]) {
+//         counter++;
+//       }
+//     }
+//     if (counter === input.length - 1 && i === result.length) {
+//       result += char;
+//     }
+//   }
+//   return result;
+// };
+
+// Solution 2 using higher order function
+
+const longestCommonPrefix = () => {
+  return input.reduce((prev, next) => {
+    let i = 0;
+    while (prev[i] && next[i] && prev[i] === next[i]) i++;
+    return prev.slice(0, i);
+  });
+};
+
+console.log(longestCommonPrefix());
