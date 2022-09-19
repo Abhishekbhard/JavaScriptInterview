4dc1a57588f828840a096234749514bae2e9e037
Abhishek <eduandabhi@gmail.com>
2022-09-19 12:17:57 +0530

    With Callback

--- CallBack/callbackExample.js
+++ CallBack/callbackExample.js
@@ -1,22 +1,45 @@
 // Without callBack
-function doStep1(init) {
-  return init + 1;
-}
+// function doStep1(init) {
+//   return init + 1;
+// }
 
-function doStep2(init) {
-  return init + 2;
-}
-function doStep3(init) {
-  return init + 3;
-}
+// function doStep2(init) {
+//   return init + 2;
+// }
+// function doStep3(init) {
+//   return init + 3;
+// }
 
+// function doOperation() {
+//   let result = 0;
+//   result = doStep1(result);
+//   result = doStep2(result);
+//   result = doStep3(result);
+
+//   console.log(`result: ${result}`);
+// }
+
+// doOperation();
+
+function doStep1(init, callback) {
+  const result = init + 1;
+  callback(result);
+}
+function doStep2(init, callback) {
+  const result = init + 2;
+  callback(result);
+}
+function doStep3(init, callback) {
+  const result = init + 3;
+  callback(result);
+}
 function doOperation() {
-  let result = 0;
-  result = doStep1(result);
-  result = doStep2(result);
-  result = doStep3(result);
-
-  console.log(`result: ${result}`);
+  doStep1(0, (result1) => {
+    doStep2(result1, (result2) => {
+      doStep3(result2, (result3) => {
+        console.log(`result: ${result3}`);
+      });
+    });
+  });
 }
-
 doOperation();
