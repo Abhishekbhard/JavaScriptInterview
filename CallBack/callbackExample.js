4cc2ccb7e41551b199a7eadce07721d2c8e93834
Abhishek <eduandabhi@gmail.com>
2022-09-19 11:55:18 +0530

    Without Callback

---
+++ CallBack/callbackExample.js
@@ -0,0 +1,22 @@
+// Without callBack
+function doStep1(init) {
+  return init + 1;
+}
+
+function doStep2(init) {
+  return init + 2;
+}
+function doStep3(init) {
+  return init + 3;
+}
+
+function doOperation() {
+  let result = 0;
+  result = doStep1(result);
+  result = doStep2(result);
+  result = doStep3(result);
+
+  console.log(`result: ${result}`);
+}
+
+doOperation();
