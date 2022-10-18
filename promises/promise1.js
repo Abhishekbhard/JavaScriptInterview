5e9bc805fe268b439e7fe851af7d502202cd4c0c
Abhishek <eduandabhi@gmail.com>
2022-10-18 17:52:13 +0530

    Promise handling

---
+++ promises/promise1.js
@@ -0,0 +1,51 @@
+const THRESHOLD_A = 0;
+
+function tettheredGetNumber(resolve, reject) {
+  setTimeout(() => {
+    const randomInt = Date.now();
+    const value = randomInt % 10;
+    if (value < THRESHOLD_A) {
+      resolve(value);
+    } else {
+      reject(`To large: ${value}`);
+    }
+  }, 500);
+}
+
+function determineParity(value) {
+  const isOdd = value % 2 === 1;
+  return { value, isOdd };
+}
+function troubleWithGetNumber(reason) {
+  const err = new Error("Trouble getting number");
+  console.error(err);
+  throw err;
+}
+
+function promiseGetWord(parityInfo) {
+  return new Promise((resolve, reject) => {
+    const { value, isOdd } = parityInfo;
+    if (value >= THRESHOLD_A - 1) {
+      reject(`Still too large: ${value}`);
+    } else {
+      parityInfo.wordEvenOdd = isOdd ? "odd" : "even";
+      resolve(parityInfo);
+    }
+  });
+}
+
+new Promise(tettheredGetNumber)
+  .then(determineParity, troubleWithGetNumber)
+  .then(promiseGetWord)
+  .then((info) => {
+    console.log(`Got: ${info.value}, ${info.wordEvenOdd}`);
+    return info;
+  })
+  .catch((reason) => {
+    if (reason.cause) {
+      console.error("Had Previously handled error");
+    } else {
+      console.error(`Trouble with promiseGetWord(): ${reason}`);
+    }
+  })
+  .finally((info) => console.log("ALL DONE"));
