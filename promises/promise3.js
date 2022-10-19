1b5a9b802acb57a135421ce42badce9bc49478fe
Abhishek <eduandabhi@gmail.com>
2022-10-19 12:10:47 +0530

    Promise all

--- promises/promise3.js
+++ promises/promise3.js
@@ -5,19 +5,25 @@ function sleepRandom(time) {
 }
 console.log("Run");
 
-sleepRandom(5)
-  .then((result) => {
-    console.log(result);
-    return sleepRandom(10);
-  })
-  .then((result) => {
-    console.log(result);
-  })
-  .then(() => {
-    console.log("immediately after");
-    throw new Error("Irror");
-  })
-  .then(() => {
-    console.log("will not be displayed");
-  })
-  .catch((x) => console.log(x));
+// sleepRandom(5)
+//   .then((result) => {
+//     console.log(result);
+//     return sleepRandom(10);
+//   })
+//   .then((result) => {
+//     console.log(result);
+//   })
+//   .then(() => {
+//     console.log("immediately after");
+//     throw new Error("Irror");
+//   })
+//   .then(() => {
+//     console.log("will not be displayed");
+//   })
+//   .catch((x) => console.log(x));
+Promise.all([
+  "foo",
+  sleepRandom(5),
+  sleepRandom(15),
+  sleepRandom(10), // after 15 sec:
+]).then((x) => console.log(x)); // => ['foo', 956, 85, 382]
